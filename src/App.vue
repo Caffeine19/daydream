ab5e28edfa5397801c1e1a6366f2077f04b49e00
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2023-09-20 20:53:38 +0800

    :construction: WIP

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <script setup lang="ts">
+import { FlagIcon } from '@heroicons/vue/24/solid'
 </script>
 
 <template>
-<div>123</div>
+  <div class="container mx-auto">
+    <div class="flex flex-col items-center p-8 border-neutral-200 border space-y-1">
+      <div class="px-3 py-2 bg-red-600/10 rounded-full flex items-center space-x-1 text-red-600">
+        <FlagIcon class="w-6 h-6"></FlagIcon>
+        <p class="text-lg font-medium">Begin</p>
+      </div>
+      <div class="h-12 w-[2px] bg-neutral-400"></div>
+      <div class="px-3 py-2 border border-neutral-400 rounded-lg">
+        <p class="text-neutral-600 text-base font-normal">2,000</p>
+      </div>
+      <div class="h-12 w-[2px] bg-neutral-400"></div>
+      <div class="px-3 py-2 border border-neutral-400 rounded-lg">
+        <p class="text-neutral-600 text-base font-normal">2,000</p>
+      </div>
+      <div class="h-12 w-[2px] bg-neutral-400"></div>
+      <div class="px-3 py-2 border border-neutral-400 rounded-lg">
+        <p class="text-neutral-600 text-base font-normal">2,000</p>
+      </div>
+      <div class="h-12 w-[2px] bg-neutral-400"></div>
+      <div class="px-3 py-2 border border-neutral-400 rounded-lg">
+        <p class="text-neutral-600 text-base font-normal">2,000</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
